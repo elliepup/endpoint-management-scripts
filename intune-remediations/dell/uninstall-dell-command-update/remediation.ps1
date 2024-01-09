642bcc8f23722a8fadae947d6bbc0019b193c1a7
Nicholas Tabb <49000519+elliepup@users.noreply.github.com>
2024-01-09 15:35:35 -0500

    Added DCU (Dell) uninstall remediation script

--- intune-remediations/dell/uninstall-dell-command-update/remediation.ps1
+++ intune-remediations/dell/uninstall-dell-command-update/remediation.ps1
@@ -1,11 +1,12 @@
-# File: detection.ps1
-# Description: This checks for the presence of Dell Command Update. To be used in conjunction with uninstall-dell-command-update/remediation.ps1.
+# File: remedation.ps1
+# Description: This script uninstalls Dell Command Update. To be used in conjunction with uninstall-dell-command-update/detection.ps1.
 # Author: Nicholas Tabb
 # Date: 01/09/2023
 # Version: 1.0 - Initial Release
 
 # get uninstall string from registry; can either be 32-bit or 64-bit
 $displayName = "Dell Command | Update*"
+
 function Get-UninstallString { 
     param(
         [Parameter(Mandatory = $true)]
@@ -28,12 +29,25 @@ function Get-UninstallString {
 
 $uninstallString = Get-UninstallString -DisplayName $displayName
 
-# if uninstall string is not null, exit 1 indicating remediation is applicable
+# if uninstall string is not null, uninstall Dell Command Update
 if ($uninstallString) {
-    Write-Host "Dell Command Update is installed. Remediation is applicable."
+    try {
+        $uninstallString = ($uninstallString.Replace("/I", "/X").Split(" "))[1]
+        Start-Process -FilePath "msiexec.exe" -ArgumentList "$uninstallString /qn" -Wait
+    }
+    catch {
+        Write-Host "Error: $_"
+        exit 1
+    }
+}
+
+# double check that Dell Command Update is uninstalled
+$uninstallString = Get-UninstallString -DisplayName $displayName
+
+if ($uninstallString) {
+    Write-Host "Dell Command Update is still installed. Remediation failed."
     exit 1
 }
 
-# if uninstall string is null, exit 0 indicating remediation is not applicable
-Write-Host "Dell Command Update is not installed. Remediation is not applicable."
+Write-Host "Dell Command Update is uninstalled. Remediation succeeded."
 exit 0
