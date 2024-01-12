435ef37cd3eb554a72a1579acc7feb7f69b8d8a1
Nicholas Tabb <49000519+elliepup@users.noreply.github.com>
2024-01-12 14:38:49 -0500

    Added remediation for SCCM cache clear remediation

--- intune-remediations/delete-sccm-cache/remediation.ps1
+++ intune-remediations/delete-sccm-cache/remediation.ps1
@@ -1,5 +1,5 @@
-# File: detection.ps1
-# Description: This is the detection logic for the delete-sccm-cache Intune remediation.
+# File: remediation.ps1
+# Description: This is the remediation logic for the delete-sccm-cache Intune remediation.
 # Author: Nicholas Tabb
 # Date: 01/12/2023
 # Version: 1.0 - Initial Release
@@ -24,15 +24,30 @@ function Get-SCCMCache {
     }
 }
 
+function Remove-SCCMCache {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$SCCMCachePath
+    )
+
+    try {
+        # delete all files and folders in SCCM cache
+        Get-ChildItem -Path $SCCMCachePath -Force | Remove-Item -Force -Recurse
+    }
+    catch {
+        Write-Host "Error: $_"
+        exit 1
+    }
+}
+
 # get SCCM cache
 $sccmCache = Get-SCCMCache -SCCMCachePath $sccmCachePath
 
-# if SCCM cache is empty, exit 0; else, exit 1 to trigger remediation
+# if SCCM cache is empty, exit 0; else, delete SCCM cache
 if ($sccmCache) {
-    Write-Host "SCCM cache is not empty. Remediation required."
-    exit 1
+    Write-Host "SCCM cache is not empty. Deleting SCCM cache."
+    Remove-SCCMCache -SCCMCachePath $sccmCachePath
 }
 else {
     Write-Host "SCCM cache is empty. No remediation required."
-    exit 0
 }
