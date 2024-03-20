6f02f16aa10399922b445e50c1d722526adcbbae
Nicholas Tabb <49000519+elliepup@users.noreply.github.com>
2024-03-20 12:35:47 -0400

    Add detection for Lenovo High Performance mode

--- intune-remediations/lenovo/disable-lenovo-high-performance/detection.ps1
+++ intune-remediations/lenovo/disable-lenovo-high-performance/detection.ps1
@@ -7,21 +7,52 @@
 
 # --------------------------- Modify As Necessary ------------------------------- #
 $properties = @(
-    "AdaptiveThermalManagementAC
-    AdaptiveThermalManagementBattery"
+    "AdaptiveThermalManagementAC", # specify the BIOS property for Adaptive Thermal Management when on AC power
+    "AdaptiveThermalManagementBattery" # specify the BIOS property for Adaptive Thermal Management when on battery power
 )
-$desiredState = "Balanced"
+$desiredState = "Balanced" # specify the desired state for the BIOS property
 # ------------------------------------------------------------------------------- #
 
+
+# function to get Lenovo BIOS settings based on specified properties
 function Get-LenovoBiosSetting {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
-        [string]$Property
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string[]]$Property
     )
 
-    $getBiosSetting = Get-WmiObject -Class Lenovo_BiosSetting -Namespace root\wmi
-    $biosSetting = $getBiosSetting | Where-Object { $_.CurrentSetting -like "$property,*" }
+    begin {
+        $getBiosSetting = Get-WmiObject -Class Lenovo_BiosSetting -Namespace root\wmi
+        $results = @()
+    }
 
-    return $biosSetting
+    process {
+        # filter the BIOS settings based on the specified property
+        $results += $getBiosSetting | Where-Object { $_.CurrentSetting -like "$Property,*" }
+    }
+
+    end {
+        return $results
+    }
+
 }
+
+# if device is not lenovo, exit 0 to indicate that the device will not be remediated
+if (-not (Get-WmiObject -Class Win32_ComputerSystem | Where-Object { $_.Manufacturer -like "Lenovo*" })) {
+    Write-Host "Device is not a Lenovo. Remediation is not applicable."
+    exit 0
+}
+
+$properties | Get-LenovoBiosSetting | ForEach-Object {
+    # split the string on the comma and get the second element
+    $currentSetting = $_.CurrentSetting.Split(",")[1]
+    
+    # if the current setting is not equal to the desired state, exit 1
+    if ($currentSetting -ne $desiredState) {
+        Write-Host "Computer is not in desired state. `nProperty: $($_.CurrentSetting.Split(",")[0]) `nCurrent State: $currentSetting `nDesired State: $desiredState"
+        exit 1
+    }
+}
+
+exit 1
